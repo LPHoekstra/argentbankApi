c5fed4f5ee0438314de441114ed747f78d5f6c1e
MalBourbie <MalBourbie@gmail.com>
2025-03-06 12:31:25 +0100

    isBlacklistedWithSuccess test.

--- src/test/java/com/argentbank/argentbankApi/jwtBlacklistService/IsBlackListedTest.java
+++ src/test/java/com/argentbank/argentbankApi/jwtBlacklistService/IsBlackListedTest.java
@@ -1,5 +1,38 @@
 package com.argentbank.argentbankApi.jwtBlacklistService;
 
-public class IsBlackListedTest {
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
 
+import java.util.Date;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.argentbank.argentbankApi.service.JwtBlacklistService;
+
+@SpringBootTest
+public class IsBlackListedTest {
+    private JwtBlacklistService jwtBlacklistService;
+    private ConcurrentHashMap<String, Long> blacklistMock = spy(new ConcurrentHashMap<>());
+
+    @BeforeEach
+    void setUp() {
+        jwtBlacklistService = new JwtBlacklistService(blacklistMock);
+    }
+
+    @Test
+    void isBlacklistedWithSuccess() throws Exception {
+        String token = "test.token";
+        Long expirationTime = new Date().getTime() + 60 * 1000L;
+
+        when(blacklistMock.get(token)).thenReturn(expirationTime);
+
+        // act
+        Boolean isBlacklisted = jwtBlacklistService.isBlackListed(token);
+
+        assertTrue(isBlacklisted, "Token must be in the blacklist");
+    }
 }
